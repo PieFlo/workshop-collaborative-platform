db7f6601cb53a2b5849d877d073ec824b978a84c
pfpouj <pf.poujol@gmail.com>
2018-09-13 16:57:31 +0200

    listing recette ok

--- php/Recette_Bonne_Adresse/ListingRecette.php
+++ php/Recette_Bonne_Adresse/ListingRecette.php
@@ -80,7 +80,7 @@ if($row = $response->fetch()) {
                 $reponse->closeCursor(); // Termine le traitement de la requête
                 ?>
             </table>
-            <a href="DisplayRecette.php.php">Mes recettes</a>
+            <a href="DisplayRecette.php">Mes recettes</a>
         </div>
     </div>
 </div>
